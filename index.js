c786f5c491888319d9dad70f283bf75edd101538
Zack Njine Wachira <zackwachira45@gmail.com>
2024-07-15 14:59:33 +0300

    Adds index.html index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-let a = 2000
-let b = 100
-
+{
+let a = 2000;
+let b = 100;
 function divide(a, b) {
     return a / b
-}
+}}
 
-let x = 2
+let x = 2;
 
 function square(x) {
-    return x * x
+    return x * x;
 }
 
-let a = 3
-let b = 4
+let a = 3;
+let b = 4;
 function add(a, b) {
-    return a + b
+    return a + b;
 }
